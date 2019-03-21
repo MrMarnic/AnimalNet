ede5a8801914bfd6dd17121859f3d083fa28c3f8
Marcel <marcel.jedig@web.de>
2019-03-21 18:50:31 +0100

    + Build system updated

--- src/main/java/me/marnic/animalnet/main/AnimalNet.java
+++ src/main/java/me/marnic/animalnet/main/AnimalNet.java
@@ -16,7 +16,7 @@ public class AnimalNet
 {
     public static final String MODID = "animalnet";
     public static final String NAME = "Animal Net";
-    public static final String VERSION = "1.8";
+    public static final String VERSION = "1.9";
     public static final AnimalNetModHandler ANIMAL_NET_MOD_HANDLER = new AnimalNetModHandler();
 
 
