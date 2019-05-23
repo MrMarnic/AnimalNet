a4fe8245fe01044da7b9525f3ebaabb10e28c757
MrMarnic <marceljedig@web.de>
2019-05-23 21:06:33 +0200

    + Added build support

--- src/main/java/me/marnic/animalnet/main/AnimalNet.java
+++ src/main/java/me/marnic/animalnet/main/AnimalNet.java
@@ -13,7 +13,7 @@ import org.apache.logging.log4j.Logger;
 public class AnimalNet {
     public static final String MODID = "animalnet";
     public static final String NAME = "Animal Net";
-    public static final String VERSION = "1.11";
+    public static final String VERSION = "1.12";
     public static final AnimalNetModHandler ANIMAL_NET_MOD_HANDLER = new AnimalNetModHandler();
 
 
