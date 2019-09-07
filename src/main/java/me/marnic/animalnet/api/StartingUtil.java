b058b1b83613439b54482bc00ca31dfcec073c3b
MrMarnic <marceljedig@web.de>
2019-09-07 13:28:19 +0200

    + Fixed Linux bug

--- src/main/java/me/marnic/animalnet/api/StartingUtil.java
+++ src/main/java/me/marnic/animalnet/api/StartingUtil.java
@@ -9,7 +9,7 @@ import java.io.File;
  */
 public class StartingUtil {
     public static void handleServerStart(File gameDir) {
-        File animalData = new File(gameDir.getAbsolutePath() + "\\animalData");
+        File animalData = new File(gameDir.getAbsolutePath() + File.separator + "animalData");
         if (!animalData.exists()) {
             animalData.mkdir();
         }
